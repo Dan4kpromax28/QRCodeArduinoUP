726ddbbfa3998f11e08cfd574919d26449335b6e
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2025-03-06 22:24:33 +0200

    New: Network connection

--- QRCodeArduino.ino
+++ QRCodeArduino.ino
@@ -1,10 +1,23 @@
 #include <HardwareSerial.h>
+#include <WiFi.h>
+#include "config.h"
 
+#define QR_RX 16
+#define QR_TX 17
 HardwareSerial mySerial(1); 
 
 void setup() {
   Serial.begin(9600);  
-  mySerial.begin(9600, SERIAL_8N1, 16, 17);  
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+
+  }
+  Serial.print("\nVeiksmigais pieslegums");
+
+  mySerial.begin(9600, SERIAL_8N1, QR_RX, QR_TX);  
 }
 
 void loop() {
